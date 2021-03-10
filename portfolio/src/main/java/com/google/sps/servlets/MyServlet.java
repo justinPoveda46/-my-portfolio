71924e39dc30db2023a11e74c2efdd30fcfe7548
Justin Poveda <jpoveda@sps-program.com>
2021-03-10 19:47:13 +0000

    Week 2: Steps 1-3

--- portfolio/src/main/java/com/google/sps/servlets/MyServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/MyServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/MyServlet")
 public class MyServlet extends HttpServlet {
-
+    private String[] myFacts = {"I am a magician", "I collect fitted hats", "I can recite the entire Nacho Libre Movie"};
     /**
      *
      */
@@ -16,7 +16,23 @@ public class MyServlet extends HttpServlet {
 
     @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String json = convertToJson(myFacts);
     response.setContentType("text/html;");
-    response.getWriter().println("Hey It's Justin");
+    response.getWriter().println(json);
+  }
+  
+  private String convertToJson(String[] myFacts) {
+    String json = "{";
+    json += "\"Fact 1\": ";
+    json += "\"" + myFacts[0] + "\"";
+    json += ", ";
+    json += "\"Fact 2\": ";
+    json += "\"" + myFacts[1] + "\"";
+    json += ", ";
+    json += "\"Fact 3\": ";
+    json += "\"" + myFacts[2] + "\"";
+    json += "}";
+    return json;
   }
 }
+  
--- portfolio/src/main/webapp/returnMessage.js
+++ portfolio/src/main/webapp/returnMessage.js
@@ -1,6 +1,8 @@
-async function returnMessage() {
+async function returnFacts() {
   const responseFromServer = await fetch('/MyServlet');
-  const textFromResponse = await responseFromServer.text();
+  const textFromResponse = await responseFromServer.json();
+  const pickFact = [textFromResponse["Fact 1"], textFromResponse["Fact 2"], textFromResponse["Fact 3"]];
+  const chooseFact = pickFact[Math.floor(Math.random() * pickFact.length)];
   const messageContainer = document.getElementById('message-container');
-  messageContainer.innerText = textFromResponse;
+  messageContainer.innerText = JSON.stringify(chooseFact);
 }
