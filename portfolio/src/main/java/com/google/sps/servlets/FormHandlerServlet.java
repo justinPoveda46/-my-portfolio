0ea343cc76c5d0b0b7b90db8758d244ad467260a
Justin Poveda <jpoveda@sps-program.com>
2021-03-11 22:20:40 +0000

    Finished Week 2 and made a new html file

---
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -0,0 +1,33 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/form-handler")
+public class FormHandlerServlet extends HttpServlet {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 6734233536872851287L;
+
+    @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    // Get the value entered in the form.
+    String textValue = request.getParameter("text-input");
+
+    // Print the value so you can see it in the server logs.
+    System.out.println("You submitted: " + textValue);
+
+    // Write the value to the response so the user can see it.
+    response.getWriter().println("You submitted: " + textValue);
+
+    // Redirects 
+    response.sendRedirect("http://jpoveda-sps-spring21.appspot.com/");
+
+  }
+}
