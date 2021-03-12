666cefb0d37f55c302461f2941a0fc86ad029c5e
Justin Poveda <jpoveda@sps-program.com>
2021-03-12 20:44:29 +0000

    fixed the indentation

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -8,26 +8,24 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/form-handler")
 public class FormHandlerServlet extends HttpServlet {
-
     /**
      *
      */
     private static final long serialVersionUID = 6734233536872851287L;
 
     @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-    // Get the value entered in the form.
-    String textValue = request.getParameter("text-input");
+        // Get the value entered in the form.
+        String textValue = request.getParameter("text-input");
 
-    // Print the value so you can see it in the server logs.
-    System.out.println("You submitted: " + textValue);
+        // Print the value so you can see it in the server logs.
+        System.out.println("You submitted: " + textValue);
 
-    // Write the value to the response so the user can see it.
-    response.getWriter().println("You submitted: " + textValue);
+        // Write the value to the response so the user can see it.
+        response.getWriter().println("You submitted: " + textValue);
 
-    // Redirects 
-    response.sendRedirect("http://jpoveda-sps-spring21.appspot.com/");
-
+        // Redirects 
+        response.sendRedirect("http://jpoveda-sps-spring21.appspot.com/");
   }
 }
